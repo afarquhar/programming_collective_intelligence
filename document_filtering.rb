0175c417c1a9bf18f499158b3766184d18c85790
Alex Farquhar <alex.farquhar@forward.co.uk>
2011-01-07 15:03:18 +0000

    bayesian filtering working

--- document_filtering.rb
+++ document_filtering.rb
@@ -1,7 +1,9 @@
-class Classifier
+class Filtering
+  
   def initialize(filename = nil)
     @feature_counts = Hash.new {|h, feature| h[feature] = Hash.new {|h, category| h[category] = 0 }}
     @category_counts = Hash.new {|h, category| h[category] = 0}
+    @thresholds = {}
   end
   
   def inc_feature(feature, category)
@@ -13,29 +15,62 @@ class Classifier
   end
   
   def feature_count(feature, category)
-    return @feature_counts[feature][category] if (@feature_counts[feature] && @feature_counts[feature][category])
-    return 0
+    return @feature_counts[feature][category].to_f if (@feature_counts[feature] && @feature_counts[feature][category])
+    return 0.0
   end
   
   def category_count(category)
-    return @category_counts[category] if @category_counts[category]
-    return 0
+    return @category_counts[category].to_f if @category_counts[category]
+    return 0.0
   end
   
   def total_count
-    @category_counts.values.inject(0) {|memo, c| memo += c}
+    @category_counts.values.inject(0) {|memo, c| memo += c}.to_f
   end
   
   def categories
     @category_counts.keys
   end
-  
-end
 
-class Filtering
+  def feature_prob(feature, category)
+    feature_count(feature, category).to_f/category_count(category).to_f
+  end
   
-  def method_missing(method, *args)
-    @classifier.send(method, *args)
+  def feature_prob_weighted(feature, category, weight = 1.0, assumed_prob = 0.5)
+    basicprob = feature_prob(feature, category)
+    total_occur = categories.map {|c| feature_count(feature, c) }.inject(0) {|memo, c| memo += c }.to_f
+    ((weight * assumed_prob) + (total_occur * basicprob))/(weight + total_occur).to_f
+  end
+  
+  def set_threshold(cat, t)
+    @thresholds[cat] = t
+  end
+  
+  def get_threshold(cat)
+    @thresholds[cat] || 1.0
+  end
+  
+  def classify(doc, default = 'unknown')
+    probs = {}
+    max = 0.0
+    best = nil
+    categories.each do |cat|
+      probs[cat] = prob(doc, cat)
+      if probs[cat] > max
+        max = probs[cat] 
+        best = cat
+      end
+      
+    end
+    
+  
+  
+    probs.each do |cat, prob|
+      next if cat == best
+      return default if ((probs[cat] * get_threshold(best)) > probs[best])
+    end
+    return best
+    
   end
   
   def sample_train
@@ -46,24 +81,35 @@ class Filtering
     train('the quick brown fox jumps', 'good')
   end
   
-  def initialize
-    @classifier = Classifier.new
+  def doc_prob(doc, category)
+    features = get_words(doc)
+    features.inject(1) {|memo, w| memo *= feature_prob_weighted(w, category) }
   end
   
+  def prob(doc, category)
+    cat_prob = category_count(category)/total_count
+    doc_prob = doc_prob(doc, category)
+    doc_prob.to_f * cat_prob
+  end
+  
+  
   def get_words(doc)
-    doc.split(/\W+/).delete_if {|w| !((2..19) === w.length) }.uniq
+    doc.split(/\W+/).delete_if {|w| !((2..19) === w.length) }.map {|w| w.downcase }.uniq
   end
   
   def train(doc, category)
     get_words(doc).each do |feature|
-      @classifier.inc_feature(feature, category)
+      inc_feature(feature, category)
     end
     
-    @classifier.inc_category(category)
+    inc_category(category)
   end
-  
-  def feature_prob(feature, category)
-    feature_count(feature, category).to_f/category_count(category)
-  end
-  
+end
+
+
+if __FILE__ == $0
+f = Filtering.new
+100.times do f.sample_train end
+  puts f.naive_bayes_prob(doc, 'good')
+  puts f.naive_bayes_prob(doc, 'bad')
 end
--- spec/filtering_spec.rb
+++ spec/filtering_spec.rb
@@ -3,21 +3,21 @@ require File.join(File.dirname(__FILE__), *%w[.. document_filtering])
 describe Filtering do
   it "should split words" do
     f = Filtering.new
-    f.get_words("aaa bbb   cc d ").should == ['aaa','bbb','cc']
+    f.get_words("aaa bbb BBB   cc d ").should == ['aaa','bbb','cc']
   end
 
 end
 
-describe Classifier do
+describe Filtering do
   it "should store category counts" do
-    c = Classifier.new
+    c = Filtering.new
     c.category_count('good').should == 0
     c.inc_category('good')
     c.category_count('good').should == 1
   end
   
   it "should store feature counts" do
-    c = Classifier.new
+    c = Filtering.new
     c.feature_count('alex', 'good').should == 0
     c.inc_feature('alex', 'good')
     c.feature_count('alex', 'good').should == 1
@@ -38,6 +38,29 @@ describe Classifier do
     f = Filtering.new
     f.sample_train
     f.feature_prob('quick', 'good').should be_close 0.66, 0.01
+    f.feature_prob_weighted('money', 'good').should be_close 0.25, 0.01
+    f.sample_train
+    f.feature_prob_weighted('money', 'good').should be_close 0.16, 0.01
+  end
+  
+  it "should find naive bayes prob" do
+    f = Filtering.new
+    f.sample_train
+    f.prob('quick rabbit', 'good').should be_close 0.156, 0.01
+    f.prob('quick rabbit', 'bad').should be_close 0.05, 0.01
+  end
+  
+  it "should classify" do
+    f = Filtering.new
+    f.sample_train
+    f.classify('quick rabbit').should == 'good'
+    f.classify('quick money').should == 'bad'
+    f.set_threshold('bad', 3.0)
+    f.classify('quick money').should == 'unknown'
+    10.times do
+       f.sample_train 
+     end
+    f.classify('quick money').should == 'bad'
     
   end
 
