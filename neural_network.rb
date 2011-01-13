ed72da439a0b32d7f2eda422ac285d591ef11a46
Alex Farquhar <alex.farquhar@forward.co.uk>
2011-01-13 15:12:49 +0000

    neural network working ok

--- neural_network.rb
+++ neural_network.rb
@@ -1,13 +1,16 @@
 require 'pstore'
+require 'fileutils'
 
 class Link
-  attr_reader :strength, :from, :to
+  attr_reader :from, :to
+  attr_accessor :strength
   
   def initialize(from, to, strength)
     @strength = strength
     @from = from 
     @to = to
   end
+  
   def to_s
     "#{@from} >---#{@strength}--> #{@to}"
   end
@@ -16,34 +19,43 @@ end
 class Network
 
   def initialize
+    FileUtils.rm('network.pstore')
+    @word_ids = []
+    @hidden_ids = []
+    @url_ids = []
+    
+    #node outputs
+    @activation_inputs = [1.0] * @word_ids.size
+    @activation_hidden = [1.0] * @hidden_ids.size
+    @activation_outputs = [1.0] * @url_ids.size    
+    
+    
     @network = PStore.new('network.pstore')
     @network.transaction do |store|
       unless store[:hidden]
         store[:hidden] = []
-        ih_hash = {}
-        ho_hash = {}
-        ih_hash.default = []
-        ho_hash.default = []
-        
-        store[:input_hidden] = ih_hash
-        store[:hidden_output] = ho_hash
+        store[:input_hidden] = {}
+        store[:hidden_output] = {}
       end
     end
   end
 
-  def generate_hidden_node(word_ids, url_ids, strength)
+  def generate_hidden_node(word_ids, url_ids)
     create_key = word_ids.sort.join("_")
-
+    
     @network.transaction do |store|
-      unless store[:hidden].include? create_key
+
+      # unless store[:hidden].include? create_key
         store[:hidden] << create_key 
         word_ids.each do |word_id|
+          store[:input_hidden][word_id] ||= []
           store[:input_hidden][word_id] << Link.new(word_id, create_key, 1.0/word_ids.size)
         end
         url_ids.each do |url_id|
-          store[:hidden_output][url_id] << Link.new(create_key, url_id, 0.1)
+          store[:hidden_output][create_key] ||= []
+          store[:hidden_output][create_key] << Link.new(create_key, url_id, 0.1)
         end
-      end
+      # end
     end
     
   end
@@ -59,6 +71,19 @@ class Network
       store[:hidden_output][hidden_id].find {|l| l.to == output_id }.strength
     end    
   end
+  
+  def set_input_hidden_strength(word_id, hidden_id, strength)
+    @network.transaction do |store|
+      store[:input_hidden][word_id].find {|l| l.to == hidden_id }.strength = strength
+    end
+    
+  end
+  
+  def set_hidden_output_strength(hidden_id, output_id, strength)
+    @network.transaction do |store|
+      store[:hidden_output][hidden_id].find {|l| l.to == output_id }.strength = strength
+    end    
+  end
 
   
   def get_all_hidden_ids(word_ids, url_ids)
@@ -68,9 +93,13 @@ class Network
       hidden = word_ids.map do |w|
         store[:input_hidden][w].map {|l| l.to }
       end
-      hidden << url_ids.map do |u|
-        store[:hidden_output][u].map {|l| l.from }
+      
+      store[:hidden_output].each do |h, links|
+        links.each do |link|
+          hidden << link.from if url_ids.include? link.to
+        end
       end
+      
     end
     # puts hidden.flatten.uniq
     hidden.flatten.uniq
@@ -82,9 +111,9 @@ class Network
     @url_ids = url_ids
     
     #node outputs
-    @activation_inputs = [1.0] * @word_ids.size
-    @activation_hidden = [1.0] * @hidden_ids.size
-    @activation_outputs = [1.0] * @url_ids.size
+    # @activation_inputs = [1.0] * @word_ids.size
+    # @activation_hidden = [1.0] * @hidden_ids.size
+    # @activation_outputs = [1.0] * @url_ids.size
     
     @weights_input = @word_ids.map do |w|
       @hidden_ids.map {|h|  get_input_hidden_strength(w, h) }
@@ -94,8 +123,8 @@ class Network
       @url_ids.map {|u|  get_hidden_output_strength(h, u) }
     end
     
-    puts @weights_input.inspect
-    puts @weights_output.inspect
+    # puts @weights_input.inspect
+    # puts @weights_output.inspect
   end
   
   def feed_forward
@@ -103,6 +132,7 @@ class Network
       #is this necessary? setting it above
       @activation_inputs[w] = 1.0
     end
+    
     
     @hidden_ids.each_index do |h|
       sum  = 0.0
@@ -124,15 +154,83 @@ class Network
     
   end
   
+
+  
+  def update
+    @word_ids.each_index do |i|
+      @hidden_ids.each_index do |j|
+        set_input_hidden_strength(@word_ids[i], @hidden_ids[j], @weights_input[i][j])
+      end
+    end
+    @hidden_ids.each_index do |j|
+      @url_ids.each_index do |k|
+        set_hidden_output_strength(@hidden_ids[j], @url_ids[k], @weights_output[j][k])
+      end
+    end
+  end
+  
   def get_result(word_ids, url_ids)
     setup_network(word_ids, url_ids)
     feed_forward
   end
   
+  def dtanh(y)
+    1.0 - y ** 2
+  end
+  
+  def back_propagate(targets, n = 0.5)
+
+    #errors for output
+    output_deltas = [0.0] * @url_ids.size
+    @url_ids.each_index do |k|
+      error = targets[k] - @activation_outputs[k]
+      output_deltas[k] =  dtanh(@activation_outputs[k]) * error
+    end
+    
+    #errors for hidden
+    hidden_deltas = [0.0] * @hidden_ids.size
+    @hidden_ids.each_index do |j|
+      error = 0.0
+      @url_ids.each_index do |k|
+        error = error += output_deltas[k] * @weights_output[j][k]
+      end
+      hidden_deltas[j] = dtanh(@activation_hidden[j]) * error
+    end
+    
+    #update output weights
+    @hidden_ids.each_index do |j|
+      @url_ids.each_index do |k|
+        change = output_deltas[k] * @activation_hidden[j]
+        @weights_output[j][k] += n * change
+      end
+    end
+    
+    @word_ids.each_index do |i|
+      @hidden_ids.each_index do |j|
+        change = hidden_deltas[j] * @activation_inputs[i]
+        @weights_input[i][j] += n * change
+      end
+    end
+    
+  end
+  
+  def train_query(word_ids, url_ids, selected_url_id)
+    generate_hidden_node(word_ids, url_ids)
+    setup_network(word_ids, url_ids)
+    feed_forward
+    targets = [0.0] * url_ids.size
+    i = url_ids.index(selected_url_id)
+    targets[i] = 1.0
+    back_propagate(targets)
+    update
+  end
+  
   def print_network
+    
     @network.transaction do |store|
-      store[:input_hidden].each do |link|
-        puts "\t#{link}"
+      puts "input hidden:"
+      @word_ids.each do |w|
+        puts "\t#{store[:input_hidden][w]}"
       end
       
       puts "hidden:"
@@ -141,10 +239,9 @@ class Network
       end
       
       puts "hidden output"
-      store[:hidden_output].each do |link|
-        puts "\t#{link}"
+      @hidden_ids.each do |h|
+        puts "\t#{store[:hidden_output][h].join("\n\t")}"
       end
-
     end
   end
 
@@ -156,9 +253,14 @@ test_words = [wWorld, wBank]
 test_urls = [uWorldBank, uRiver, uEarth]
 
 n = Network.new
-n.generate_hidden_node(test_words, test_urls, 0.0)
-# n.print_network
-# puts n.get_all_hidden_ids([wWorld], [uRiver]).inspect
+# n.generate_hidden_node(test_words, test_urls, 0.0)
 # n.setup_network(test_words, test_urls)
-puts n.get_result(test_words, test_urls)
+# n.print_network
+
+# puts n.get_all_hidden_ids([wWorld], [uRiver]).inspect
+# 
+# puts n.get_result(test_words, test_urls)
+n.train_query([wWorld, wBank], [uWorldBank, uRiver, uEarth], uWorldBank)
+puts n.get_result([wWorld, wBank], [uWorldBank, uRiver, uEarth])
+n.print_network
 
